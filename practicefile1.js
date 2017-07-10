5f1aa46ec7cf35e0f4994c24487e633fc0f8cf8d
Caree Claunch <cclaunch@Carees-MBP.fios-router.home>
2017-07-09 19:18:05 -0500

    Added example to file

--- practicefile1.js
+++ practicefile1.js
@@ -1,1 +1,30 @@
 // Practicing functional JS after John's workshop!
+
+// Is this a pure function?
+// ========================================================
+
+function savingsIncrease(weeks) {
+	var originalBalance = 5000;
+	var savingsRate = 200;
+	var newBalance = originalBalance + savingsRate * weeks;
+	//console.log(newBalance);
+}
+savingsIncrease(52);
+
+
+// What about this one?!?
+// ========================================================
+function savingsIncrease(originalBalance, savingsRate, weeks) {
+	var newBalance = originalBalance + savingsRate * weeks;
+	//console.log(newBalance);
+}
+savingsIncrease(5000, 200, 52);
+
+
+// What about this one?!?
+// ========================================================
+function savingsIncrease(originalBalance, savingsRate, weeks) {
+	return originalBalance + savingsRate * weeks;
+}
+
+//console.log(savingsIncrease(5000, 200, 52));
